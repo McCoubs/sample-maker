0a869badb505cfd31a22b5d72e39eca97e4710ba
Derek Nguyen <derek.nguyen@mail.utoronto.ca>
2019-03-16 19:22:02 -0400

    woops

--- src/api/controllers/sample-routing.js
+++ src/api/controllers/sample-routing.js
@@ -14,7 +14,7 @@ module.exports = function SampleRouting(app, conn) {
     let sampleFile = req.files.sample;
     // create writestream for new file
     let writestream = gfs.createWriteStream({ filename: sampleFile.name, content_type: sampleFile.mimetype });
-
+    let readable = new stream.Readable();
     // create readable from given file, pipe into db    let readable = new stream.Readable();
     readable.push(sampleFile.data);
     readable.push(null);
