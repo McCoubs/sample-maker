bcfc77b3e0d5e54bdc4910ca9ea4f1e0786ae9fa
Spencer McCoubrey <smccoubrey@verto.ca>
2019-03-24 20:57:08 -0400

    making small code improvements

--- src/api/controllers/user-routing.js
+++ src/api/controllers/user-routing.js
@@ -2,14 +2,14 @@ let mongoose = require('mongoose');
 let User = mongoose.model('User');
 let Sample = mongoose.model('Sample');
 let Subscription = mongoose.model('Subscription');
-let { errorGenerator, jwtAuth } = require('../helpers');
+let { jwtAuth, errorGenerator } = require('../helpers');
 
 module.exports = function UserRouting(app) {
 
   app.post('/api/register', (req, res) => {
     User.findOne({email: req.body.email}, (err, foundUser) => {
-      if (err) return res.status(500).json(errorGenerator(err, 500, 'Error creating new user with email: ' + req.body.email));
-      if (foundUser) return res.status(400).json(errorGenerator(err, 400, 'A user already exists with that email'));
+      if (err) return errorGenerator(res, err, 500, 'Error creating new user with email: ' + req.body.email);
+      if (foundUser) return errorGenerator(res, null, 400, 'A user already exists with that email');
 
       let user = new User();
       // set new user info
@@ -19,7 +19,7 @@ module.exports = function UserRouting(app) {
 
       // attempt to save new user; on error => error else => return token
       user.save(function(err) {
-        if (err) return res.status(500).json(errorGenerator(err, 500, 'Error creating new user with email: ' + req.body.email));
+        if (err) return errorGenerator(res, err, 500, 'Error creating new user with email: ' + req.body.email);
         // add token to cookies and respond with token
         const generatedJWT = user.generateJwt();
         const inProd = process.env.NODE_ENV === 'production';
@@ -33,10 +33,10 @@ module.exports = function UserRouting(app) {
     // try to find user by email
     User.findOne({ email: req.body.email }, (err, user) => {
       // return error on error or no user
-      if (err || !user) return res.status(500).json(errorGenerator(err, 500, 'Cannot find user with provided email'));
+      if (err || !user) return errorGenerator(res, err, 500, 'Cannot find user with provided email');
       // Return if password is wrong
       if (!user.validPassword(req.body.password)) {
-        return res.status(401).json(errorGenerator(err, 401, 'Failed to login. Email and/or password are incorrect'));
+        return errorGenerator(res, err, 401, 'Failed to login. Email and/or password are incorrect');
       }
       // add token to cookies and respond with token
       const generatedJWT = user.generateJwt();
@@ -55,11 +55,11 @@ module.exports = function UserRouting(app) {
 
   app.get('/api/users/:id', jwtAuth, (req, res) => {
     if (!req.params.id) {
-      return res.status(400).json(errorGenerator(null, 400, 'Invalid request, ID not provided'));
+      return errorGenerator(res, null, 400, 'Invalid request, ID not provided');
     } else {
       // Otherwise continue
       User.findById(req.params.id).exec((err, user) => {
-        if (err || !user) return res.status(500).json(errorGenerator(err, 500, 'no user found with id: ' + req.params.id));
+        if (err || !user) return errorGenerator(res, err, 500, 'no user found with id: ' + req.params.id);
         res.status(200).json(user);
       });
     }
@@ -67,10 +67,10 @@ module.exports = function UserRouting(app) {
 
   app.get('/api/user/:id/samples',jwtAuth, (req, res) => {
     if (!req.params.id) {
-      return res.status(400).json(errorGenerator(null, 400, 'Invalid request, ID not provided'));
+      return errorGenerator(res, null, 400, 'Invalid request, ID not provided');
     } else {
-      Sample.find({author: req.params.id}, (err, samples) =>{
-        if (err) return res.status(500).json(errorGenerator(err,500, 'Could not find samples for user: ' + req.params.id));
+      Sample.find({ author: req.params.id }, (err, samples) =>{
+        if (err) return errorGenerator(res, err, 500, 'Could not find samples for user: ' + req.params.id);
         res.json(samples);
       });
     }
@@ -80,23 +80,23 @@ module.exports = function UserRouting(app) {
   app.post('/api/subscribe',jwtAuth, (req, res) => {
     // Subscription.insertOne({follower: id1, followee: id2});
     if (!req.params.id) {
-      return res.status(400).json(errorGenerator(null, 400, 'Invalid request, ID not provided'));
+      return errorGenerator(res, null, 400, 'Invalid request, ID not provided');
     }
   });
 
   app.post('/api/unsubscribe',jwtAuth, (req, res) => {
     // Subscription.deleteOne({follower: id1, followee: id2});
     if (!req.params.id) {
-      return res.status(400).json(errorGenerator(null, 400, 'Invalid request, ID not provided'));
+      return errorGenerator(res, null, 400, 'Invalid request, ID not provided');
     }
   });
 
   app.get('/api/user/:id/subscribers',jwtAuth, (req, res) => {
     if (!req.params.id) {
-      return res.status(400).json(errorGenerator(null, 400, 'Invalid request, ID not provided'));
+      return errorGenerator(res, null, 400, 'Invalid request, ID not provided');
     } else {
       Subscription.find({followee: req.params.id}, (err, subscribers) =>{
-        if (err) return res.status(500).json(errorGenerator(err,500, 'Could not find subscribers for user: ' + req.params.id));
+        if (err) return errorGenerator(res, err, 500, 'Could not find subscribers for user: ' + req.params.id);
         res.json(subscribers);
       });
     }
@@ -104,10 +104,10 @@ module.exports = function UserRouting(app) {
 
   app.get('/api/user/:id/subscriptions',jwtAuth, (req, res) => {
     if (!req.params.id) {
-      return res.status(400).json(errorGenerator(null, 400, 'Invalid request, ID not provided'));
+      return errorGenerator(res, null, 400, 'Invalid request, ID not provided');
     } else {
       Subscription.find({follower: req.params.id}, (err, subscriptions) =>{
-        if (err) return res.status(500).json(errorGenerator(err,500, 'Could not find subscriptions for user: ' + req.params.id));
+        if (err) return errorGenerator(err,500, 'Could not find subscriptions for user: ' + req.params.id);
         res.json(subscriptions);
       });
     }
--- src/app/global-services/authentication.service.ts
+++ src/app/global-services/authentication.service.ts
@@ -24,7 +24,7 @@ export class AuthenticationService {
     // get current user and check expiry token
     const user = this.userService.getCurrentUser();
     if (!isNullOrUndefined(user)) {
-      return user.exp > Date.now() / 1000;
+      return user.exp > (Date.now() / 1000);
     } else {
       return false;
     }
